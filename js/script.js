b87073b46118cdddd2ad933a37812044f55ef5de
Giadaspt <saputo.giada@gmail.com>
2021-11-02 21:56:38 +0100

    square display

--- js/script.js
+++ js/script.js
@@ -5,97 +5,54 @@ creare la griglia
 - faccio l'append al container
 */
 
-const container = document.querySelector('.container');
-let button = document.querySelector('.btn');
+let container = document.querySelector('.container');
+let square = document.getElementsByClassName("square");
 
 
-// makeGrid1(container);
-// makeGrid2(container);
-// makeGrid3(container);
 
-let dropDown = document.getElementById('windowValue').value= '';
 
-button.addEventListener('click', function(){
+function GetSelectedValue(){
+  let e = document.getElementById("windowValue");
+  let result = e.options[e.selectedIndex].value;
+  // document.getElementById("result").innerHTML = result;
+  console.log(result);
 
-  if (dropDown.value = '1'){
-    makeGrid1(container);
+  let square = document.getElementsByClassName("square");
 
+  if (result == '10') {
+    square.classList.add("active");
   }
-  if (dropDown.value = '2'){
-    makeGrid2(container);
-
-  }
-  if (dropDown.value = '3'){
-    makeGrid3(container);
-
-  }
-})
-
-
-
-
-function makeGrid1(numb_grid){
   
-  for(let i = 0; i < 10; i++ ) {
-    let square = document.createElement('div');
-    square.className = 'square';
-    square.style.width = 'calc(100% / 10)';
-    square.style.height = 'calc(100% / 10)';
-    for(let k = 0; k < 9; k++ ) {
-      let square = document.createElement('div');
-      square.className = 'square';
-      square.style.width = 'calc(100% / 10)';
-      square.style.height = 'calc(100% / 10)';
-      numb_grid.append(square);
-    }
-    numb_grid.append(square);
+  if (result == '9'){
+    square.classList.add("active");
   }
-  console.log('10 colonne e righe');
-
-}
-
-
-function makeGrid2(numb_grid){
+  if (result == '7'){
+    square.classList.add("active");
+  }
   
 
-  for(let i = 0; i < 9; i++ ) {
+  makeGrid(result, container);
+}
+
+
+function makeGrid(numb, numb_grid){
+  
+  for(let i = 0; i < numb; i++ ) {
     let square = document.createElement('div');
     square.className = 'square';
-    square.style.width = 'calc(100% / 9)';
-    square.style.height = 'calc(100% / 9)';
-    for(let k = 0; k < 8; k++ ) {
+    square.style.width = `calc(100% / ${numb})`;
+    square.style.height = `calc(100% / ${numb})`;
+    for(let k = 0; k < numb -1; k++ ) {
       let square = document.createElement('div');
       square.className = 'square';
-      square.style.width = 'calc(100% / 9)';
-      square.style.height = 'calc(100% / 9)';
+      square.style.width = `calc(100% / ${numb})`;
+      square.style.height = `calc(100% / ${numb})`;
       numb_grid.append(square);
     }
     numb_grid.append(square);
   }
-  console.log('9 colonne e righe');
+  console.log('colonne e righe');
 
 }
-
-function makeGrid3(numb_grid){
- 
-
-  for(let i = 0; i < 7; i++ ) {
-    let square = document.createElement('div');
-    square.className = 'square';
-    square.style.width = 'calc(100% / 7)';
-    square.style.height = 'calc(100% / 7)';
-    for(let k = 0; k < 6; k++ ) {
-      let square = document.createElement('div');
-      square.className = 'square';
-      square.style.width = 'calc(100% / 7)';
-      square.style.height = 'calc(100% / 7)';
-      numb_grid.append(square);
-    }
-    numb_grid.append(square);
-  }
-  console.log('7 colonne e righe');
- 
-}
-
 
 
