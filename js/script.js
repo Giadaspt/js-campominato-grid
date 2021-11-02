50906da92db436ed0c2eb5cf63d1a2d1d84e1fd6
Giadaspt <saputo.giada@gmail.com>
2021-11-02 19:45:05 +0100

    in progress

--- js/script.js
+++ js/script.js
@@ -6,23 +6,96 @@ creare la griglia
 */
 
 const container = document.querySelector('.container');
-let square = makeGrid(container);
-console.log(container);
+let button = document.querySelector('.btn');
 
 
-function makeGrid(numb_grid){
+// makeGrid1(container);
+// makeGrid2(container);
+// makeGrid3(container);
 
-  for(let i = 0; i < 49; i++ ) {
+let dropDown = document.getElementById('windowValue').value= '';
+
+button.addEventListener('click', function(){
+
+  if (dropDown.value = '1'){
+    makeGrid1(container);
+
+  }
+  if (dropDown.value = '2'){
+    makeGrid2(container);
+
+  }
+  if (dropDown.value = '3'){
+    makeGrid3(container);
+
+  }
+})
+
+
+
+
+function makeGrid1(numb_grid){
+  
+  for(let i = 0; i < 10; i++ ) {
     let square = document.createElement('div');
     square.className = 'square';
-    square.style.width = '80px';
-    square.style.height = '80px';
+    square.style.width = 'calc(100% / 10)';
+    square.style.height = 'calc(100% / 10)';
+    for(let k = 0; k < 9; k++ ) {
+      let square = document.createElement('div');
+      square.className = 'square';
+      square.style.width = 'calc(100% / 10)';
+      square.style.height = 'calc(100% / 10)';
+      numb_grid.append(square);
+    }
     numb_grid.append(square);
   }
-  console.log(numb_grid);
+  console.log('10 colonne e righe');
+
 }
 
 
+function makeGrid2(numb_grid){
+  
+
+  for(let i = 0; i < 9; i++ ) {
+    let square = document.createElement('div');
+    square.className = 'square';
+    square.style.width = 'calc(100% / 9)';
+    square.style.height = 'calc(100% / 9)';
+    for(let k = 0; k < 8; k++ ) {
+      let square = document.createElement('div');
+      square.className = 'square';
+      square.style.width = 'calc(100% / 9)';
+      square.style.height = 'calc(100% / 9)';
+      numb_grid.append(square);
+    }
+    numb_grid.append(square);
+  }
+  console.log('9 colonne e righe');
+
+}
+
+function makeGrid3(numb_grid){
+ 
+
+  for(let i = 0; i < 7; i++ ) {
+    let square = document.createElement('div');
+    square.className = 'square';
+    square.style.width = 'calc(100% / 7)';
+    square.style.height = 'calc(100% / 7)';
+    for(let k = 0; k < 6; k++ ) {
+      let square = document.createElement('div');
+      square.className = 'square';
+      square.style.width = 'calc(100% / 7)';
+      square.style.height = 'calc(100% / 7)';
+      numb_grid.append(square);
+    }
+    numb_grid.append(square);
+  }
+  console.log('7 colonne e righe');
+ 
+}
 
 
 
