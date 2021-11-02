6ebb9d4e6067d33c51227ce6df5dff509a3865ee
Giadaspt <saputo.giada@gmail.com>
2021-11-02 15:39:28 +0100

    49 square

--- js/script.js
+++ js/script.js
@@ -10,17 +10,19 @@ let square = makeGrid(container);
 console.log(container);
 
 
-
 function makeGrid(numb_grid){
 
-  for(let i = 0; i < 20; i++ ) {
+  for(let i = 0; i < 49; i++ ) {
     let square = document.createElement('div');
     square.className = 'square';
-    square.style.width = 40 ;
-    square.style.height = 40 ;
+    square.style.width = '80px';
+    square.style.height = '80px';
     numb_grid.append(square);
   }
   console.log(numb_grid);
 }
+
+
+
 
 
