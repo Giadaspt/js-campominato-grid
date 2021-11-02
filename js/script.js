b1097f30ea7cbf233da4363a02b0b9ffe21f96cd
Giadaspt <saputo.giada@gmail.com>
2021-11-02 15:20:09 +0100

    griglia

--- js/script.js
+++ js/script.js
@@ -1,8 +1,26 @@
 
 /*
-1. ho un continer a 
-
-
+creare la griglia
+- creo una classe (square)
+- faccio l'append al container
 */
 
 const container = document.querySelector('.container');
+let square = makeGrid(container);
+console.log(container);
+
+
+
+function makeGrid(numb_grid){
+
+  for(let i = 0; i < 20; i++ ) {
+    let square = document.createElement('div');
+    square.className = 'square';
+    square.style.width = 40 ;
+    square.style.height = 40 ;
+    numb_grid.append(square);
+  }
+  console.log(numb_grid);
+}
+
+
