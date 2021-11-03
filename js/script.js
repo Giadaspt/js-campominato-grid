a8d7c83b4f70a8ab61c8a056e1c03d940c3deec8
Giadaspt <saputo.giada@gmail.com>
2021-11-03 12:32:19 +0100

    modificato

--- js/script.js
+++ js/script.js
@@ -5,42 +5,56 @@ creare la griglia
 - faccio l'append al container
 */
 
+const play = document.querySelector('.btn').addEventListener('click', function(){
+  getSelectedValue();
+})
+
 let container = document.querySelector('.container');
 
 
 function getSelectedValue(){
-  let e = document.getElementById("windowValue");
-  let result = e.options[e.selectedIndex].value;
-
+  let result = parseInt(document.getElementById("windowValue").value);
   console.log(result);
-  
+
+  let squares;
+  let squareRow;
+
+  if (result == 1) {
+    squares = 100;
+    squareRow = 10;
+  } else if (result == 2){
+    squares = 81;
+    squareRow = 9;
+  } else if (result == 3){
+    squares = 49;
+    squareRow = 7;
+  }
+
+  console.log(squares);
+  console.log(squareRow);
+
+
   document.querySelector('.container').innerHTML = '';
+  makeGrid(container);
 
-  makeGrid(result, container);
-}
+  function makeGrid(){
 
+    let container = document.querySelector('.container');
+    
+    for(let i = 1; i < squares +1; i++ ) {
 
-function makeGrid(numb, numb_grid){
-  
-  for(let i = 0; i < numb; i++ ) {
-    let square = document.createElement('div');
-    square.className = 'square';
-    square.style.width = `calc(100% / ${numb})`;
-    square.style.height = `calc(100% / ${numb})`;
-    square.innerHTML = `<span> ${i+1} </span>`;
-    for(let k = 0; k < numb -1; k++ ) {
       let square = document.createElement('div');
       square.className = 'square';
-      square.style.width = `calc(100% / ${numb})`;
-      square.style.height = `calc(100% / ${numb})`;
-      numb_grid.append(square);
-      square.innerHTML = `<span> ${i+1} </span>`;
+      square.innerHTML = `<span> ${i} </span>`;
+      square.style.width = `calc(100% / ${squareRow})`;
+      square.style.height = `calc(100% / ${squareRow})`;
+      container.append(square);
     }
-    numb_grid.append(square);
-    
+    document.querySelector('main').append('container');
+    console.log('colonne e righe');
   }
-  console.log('colonne e righe');
 }
+
 
 
 
